a3af39acac87a9b7f69704ebbcfdf531b5431bb9
shefatmishu <shefatmishu@gmail.com>
2024-01-19 23:48:54 +0600

    extra space removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import { useState } from "react";
 
 function Square ({value, onSquareClick}){
-
-
-
   return (
   <button 
         className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg"
